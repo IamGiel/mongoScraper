f499b7b86741837e52798781c096db26cd12636b
Server <november14giel@gmail.com>
2018-02-18 13:40:59 -0500

    mongolab

--- server.js
+++ server.js
@@ -34,6 +34,16 @@ mongoose.Promise = Promise;
 mongoose.connect("mongodb://localhost/mongoscraper");
 var db = mongoose.connection;
 
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+    useMongoClient: true
+});
+
 // Show any mongoose errors
 db.on("error", function (error) {
     console.log("Mongoose Error: ", error);
